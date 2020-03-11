c7587c4226824b0e058c94b320c0d7e018f262b0
mohammadrezanajari <mr.najari@gmail.com>
2020-03-12 01:52:18 +0330

    reza

--- src/main/java/io/agilenintelligence/ppmtool/domin/Project.java
+++ src/main/java/io/agilenintelligence/ppmtool/domin/Project.java
@@ -1,10 +1,99 @@
 package io.agilenintelligence.ppmtool.domin;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    void reza(){
+    private long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date  startDate;
+    private Date endDate;
+    private Date createdAt;
 
-        System.out.println("reza");
+    public long getId() {
+        return id;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    private Date updatedAt;
+
+    public Project() {
+    }
+
+    @PrePersist
+    protected void onCreate(){
+
+        this.createdAt=new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt=new Date();
+    }
+
 }
